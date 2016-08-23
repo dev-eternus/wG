08b74ffe1c89fb805e5a25daa0d0682d7d8e5256
dev-eternus <siddhraj.atodaria@eternussolutions.com>
2016-08-23 15:10:27 +0530

    Update queryparser.js

--- queryparser.js
+++ queryparser.js
@@ -411,7 +411,7 @@ function(a, b) {
             },
             lang_code: "en",
             lang: {},
-            operators: ["equal", "not_equal", "in", "not_in", "less", "less_or_equal", "greater", "greater_or_equal", "between", "not_between", "begins_with", "not_begins_with", "contains", "not_contains", "ends_with", "not_ends_with", "is_empty", "is_not_empty", "is_null", "is_not_null"],
+            operators: ["equal", "not_equal", "in", "not_in", "less", "less_or_equal", "greater", "greater_or_equal", "begins_with", "not_begins_with", "contains", "not_contains", "ends_with", "not_ends_with", "is_empty", "is_not_empty", "is_null", "is_not_null"],
             icons: {
                 add_group: "glyphicon glyphicon-plus-sign",
                 add_rule: "glyphicon glyphicon-plus",
